1130d8abc70920eb19ed5916b50f3d8bd158c8e2
AkashOP <akp954834@gmail.com>
2025-07-07 11:52:15 +0530

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -11,30 +11,40 @@ def start_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"),
-            InlineKeyboardButton(text="🛡️ Pᴏʟɪᴄʏ", url="https://graph.org/Privacy-Policy-05-01-30")
+            InlineKeyboardButton(
+                text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"
+            ),
+            InlineKeyboardButton(
+                text="🛡️ Pᴏʟɪᴄʏ", url="https://graph.org/Privacy-Policy-05-01-30"
+            ),
         ],
     ]
     return buttons
+
 
 def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true"
             )
         ],
         [
-            InlineKeyboardButton(text=_["E_X_1"], url="https://github.com/NoxxOP/ShrutiMusic/fork"),
-            InlineKeyboardButton(text="🛡️ Pᴏʟɪᴄʏ", url="https://graph.org/Privacy-Policy-05-01-30")
+            InlineKeyboardButton(
+                text=_["E_X_1"], callback_data="show_video_panel"
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_5"], url=f"https://t.me/{config.OWNER_USERNAME}"
+            )
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+            InlineKeyboardButton(
+                text=_["S_B_4"], callback_data="settings_back_helper"
+            )
         ],
     ]
     return buttons
